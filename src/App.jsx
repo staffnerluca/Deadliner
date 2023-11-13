42275f9a6bc2f7c0740a6ec44c043695f1912aee
Luca Staffner <staffnerluca@gmail.com>
2023-11-13 16:13:18 +0100

    Add tasks dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,56 @@
 import { useState } from 'react'
+import ReactDOM from 'react-dom';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 
-function NewTask(){
+const tasks = []
+
+function NewTask({id}){
+  const btnId = "btn"+id;
   return(
     <div className="divTask">
-      Name: <input className="inpName" value={"Name"}></input> <input className="inpUnit" value={"Unit"}></input>
-      <input className="inpCheckbox" type="checkbox"></input> Only on work days <input type="checkbox"></input> The same every day <button>Clear line</button>
+      <input className="inpName" placeholder={"Name"}></input> 
+      <input className="inpUnit" placeholder={"Unit"}></input>
+      <input className="inpCheckbox" type="checkbox"></input> Only on work days 
+      <input type="checkbox"></input> The same every day 
+      <button id={btnId} onClick={clearLine}>Clear line</button>
     </div>
   )
 }
 
-function conectToCalendar(){
-
+function clearLine(event){
+  const btn = event.target;
+  const num = btn.id;
+  console.log(num);
+  tasks.splice(num, 1)
+  console.log(tasks.length)
 }
 
 function App() {
   const [count, setCount] = useState(0)
+  const [removed, setRemove] = useState(1)
+
+  const handleNewTaskClick = () => {
+    setCount(count+1);
+    console.log("Presed");
+    tasks.push(<NewTask key={count} id={count}/>);
+  }
+  
+  function RenderTasks(){
+    return tasks;
+  }
+
+
 
   return (
     <div>
       <h1>Deadliner</h1>
-      <button>Connect to Google calendar</button>
+      <button onClick={handleNewTaskClick}>New Task</button>
+      <br></br>
+      <br></br>
+      <RenderTasks/>
     </div>
   )
 }
